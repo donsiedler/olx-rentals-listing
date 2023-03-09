6562f4bd41f9e96d7e8cf797d435cef652d22968
Artur Siedlerski <don.siedler@gmail.com>
2023-03-09 21:19:48 +0100

    Initialise bot

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from form_bot import FormBot
 from olx_scraper import OLXScraper
 
 OLX_URL = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/"
-
+FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSekeWndUp3RzGgcjjXngAlDLkCd9GQhOZKO7mAHf3Y_vbtndg/viewform"
 
 scraper = OLXScraper(OLX_URL)
 scraper.get_listings()
 scraper.get_links()
 scraper.get_prices()
 scraper.get_addresses()
+print(scraper)
+
+bot = FormBot()
+bot.complete_form(FORM_URL)
+
