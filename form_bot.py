f30a902f1f194ff067cffbe4c73a688599b2febf
Artur Siedlerski <don.siedler@gmail.com>
2023-03-09 21:52:13 +0100

    Locate the form inputs and fill out the form

--- form_bot.py
+++ form_bot.py
@@ -1,4 +1,6 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -13,5 +15,22 @@ class FormBot:
     def __init__(self):
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    def complete_form(self, url):
+    def complete_form(self, url, listing_address, listing_price, listing_url):
         self.driver.get(url)
+
+        time.sleep(1)  # Wait for the form to load
+
+        # Locate inputs
+        address_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div['
+                                                           '2]/div/div[1]/div/div[1]/input')
+        price_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div['
+                                                         '2]/div/div[1]/div/div[1]/input')
+        url_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div['
+                                                       '2]/div/div[1]/div/div[1]/input')
+        submit_btn = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+        # Fill out the form and submit
+        address_input.send_keys(listing_address)
+        price_input.send_keys(listing_price)
+        url_input.send_keys(listing_url)
+        submit_btn.click()
