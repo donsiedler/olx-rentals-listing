48f77b7975af5125e322e8c53c818cf586011ebe
Artur Siedlerski <don.siedler@gmail.com>
2023-03-09 21:28:46 +0100

    Change scraper methods logic

--- olx_scraper.py
+++ olx_scraper.py
@@ -7,36 +7,27 @@ class OLXScraper:
         self.response = requests.get(url).text
         self.soup = BeautifulSoup(self.response, "html.parser")
         self.listings = None
-        self.links = []
-        self.prices = []
-        self.addresses = []
 
     def __str__(self):
-        return (f"Found {len(self.listings)} listings. \n"
-                f"Links ({len(self.links)}): {self.links} \n"
-                f"Prices ({len(self.prices)}): {self.prices} \n"
-                f"Addresses ({len(self.addresses)}): {self.addresses} \n")
+        return f"Found {len(self.listings)} listings."
 
     def get_listings(self):
         self.listings = self.soup.findAll(name="div", class_="css-1sw7q4x", attrs={"data-cy": "l-card"})
 
-    def get_links(self):
-        for listing in self.listings:
-            url = listing.find(name="a", class_="css-rc5s2u")["href"]
-            if url[0] == "/":
-                url = f"https://olx.pl{url}"
-            self.links.append(url)
+    def get_link(self, listing):
+        url = listing.find(name="a", class_="css-rc5s2u")["href"]
+        if url[0] == "/":
+            url = f"https://olx.pl{url}"
+        return url
 
-    def get_prices(self):
-        for listing in self.listings:
-            try:
-                price = listing.find(name="p", class_="css-10b0gli").text
-            except AttributeError:
-                price = "n/a"
-            self.prices.append(price)
+    def get_price(self, listing):
+        try:
+            price = listing.find(name="p", class_="css-10b0gli").text
+        except AttributeError:
+            price = "n/a"
+        return price
 
-    def get_addresses(self):
-        for listing in self.listings:
-            address = listing.find(name="p", class_="css-veheph", attrs={"data-testid": "location-date"})
-            address = address.text.split("-")[0].rstrip()
-            self.addresses.append(address)
+    def get_address(self, listing):
+        address = listing.find(name="p", class_="css-veheph", attrs={"data-testid": "location-date"})
+        address = address.text.split("-")[0].rstrip()
+        return address
