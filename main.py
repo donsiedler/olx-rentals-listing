437f7193da341a6b59371e142ed171a68d4185a7
Artur Siedlerski <don.siedler@gmail.com>
2023-03-09 20:12:55 +0100

    Get links from listings; add links attr to class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
+
 
 OLX_URL = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/"
 
@@ -10,14 +10,19 @@ class OLXScraper:
         self.response = requests.get(url).text
         self.soup = BeautifulSoup(self.response, "html.parser")
         self.listings = None
+        self.links = []
 
     def get_listings(self):
-        listings = self.soup.findAll(name="div", class_="css-1sw7q4x")
-        pprint(listings)
-        print(len(listings))
+        self.listings = self.soup.findAll(name="div", class_="css-1sw7q4x", attrs={"data-cy": "l-card"})
+        print(len(self.listings))
 
     def get_links(self):
-        pass
+        for listing in self.listings:
+            url = listing.find(name="a", class_="css-rc5s2u")["href"]
+            if url[0] == "/":
+                url = f"https://olx.pl{url}"
+            self.links.append(url)
+        print(self.links)
 
     def get_prices(self):
         pass
@@ -28,3 +33,4 @@ class OLXScraper:
 
 scraper = OLXScraper(OLX_URL)
 scraper.get_listings()
+scraper.get_links()
